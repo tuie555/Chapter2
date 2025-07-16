608d1df45ba9a8d54c2262b56a1c9e8f3cd5a575
ไอ่ปุณณ์ <Punnlaowatchara@gmail.com>
2025-07-16 16:45:41 +0700

    Start project2

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.adprolab2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.adprolab2 to javafx.fxml;
-    exports com.example.adprolab2;
-}
---
+++ src/main/java/se233/chapter2/Launcher.java
@@ -0,0 +1,4 @@
+package se233.chapter2;
+
+public class Launcher {
+}
